e4842897534baf7a9d171cfc43ce184d3b6319a6
ceusahin <hakonsahin@gmail.com>
2022-03-28 18:59:57 +0300

    1.ders Odev

--- src/app.js
+++ src/app.js
@@ -21,3 +21,44 @@ for(let i = 0; i<konutKredileri.length; i++){
 
 console.log("</ul>")
 
+
+// let, var, const
+let sayi1 = 10
+sayi1 = "Hakan Sahin"
+let student = {id:1, name:"Hakan"}
+// console.log(student)
+
+function save(puan = 10, ogrenci) {
+    console.log(ogrenci.name+": "+puan)
+}
+save(undefined, student);
+
+let students = ["Hakan Sahin","Emre Guclu","James Mcgrade","Busra Mutlu"]
+console.log(students)
+
+let students2 = [student,{id:2, name:"Batuhan"},"Ankara",{city:"Istanbul"}]
+console.log(students2)
+
+// rest
+let showProducts = function (id,...products) {
+    console.log(id)
+    console.log(products)
+}
+
+console.log(typeof showProducts)
+showProducts(1,"Elma","Armut")
+
+// spread
+let points = [1,3,5,6,15,33,67,899]
+console.log(...points)
+console.log(Math.max(...points))
+console.log(..."ABC","D",..."EFG","H")
+
+// destructure
+let populations = [10000,20000,30000[40000,100000]]
+let [small, medium, high, [veryHigh, maximum]] = populations
+console.log(small)
+console.log(medium)
+console.log(high)
+console.log(veryHigh)
+console.log(maximum)

be7d4c412cbe4d985c794690999af5ea5106bd2b
ceusahin <hakonsahin@gmail.com>
2022-03-28 11:35:35 +0300

    javaScript 1.ders

---
+++ src/app.js
@@ -0,0 +1,23 @@
+console.log("hello, world.")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+// JS type safe degildir.!
+console.log(dolarDun)
+
+const euroDun = 11.2
+// euroDun = 11
+console.log(euroDun)
+
+// array = dizi
+let konutKredileri = ["Konut Kredisi","Emlak Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+// console.log(konutKredileri)
+
+console.log("<ul>")
+
+for(let i = 0; i<konutKredileri.length; i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+
+console.log("</ul>")
+
